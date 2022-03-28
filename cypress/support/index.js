8c27a1a363b244fe7247ad2ca39cc6bde14863fb
Mahbub <mahbub.095@gmail.com>
2022-03-28 17:20:04 +0600

    mochareport

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -22,3 +22,12 @@ require('cypress-xpath')
 
 //faker
 cy.faker = require('faker');
+
+import addContext from "mochawesome/addContext";
+
+Cypress.on("test:after:run", (test, runnable) => {
+    if (test.state === "failed") {
+        const screenshot = `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
+        addContext({ test }, screenshot);
+    }
+});
